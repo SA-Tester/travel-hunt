755b6144c39683da757fa9706d5abb8c78606b1b
Sasith MJ <sasithmjayaweera@gmail.com>
2023-12-06 18:58:08 +0530

    django restframwork error

--- frontend/src/HelloWorld.js
+++ frontend/src/HelloWorld.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function HelloWorld() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get('<http://localhost:8000/hello-world/>')
-      .then(response => {
+    axios
+      .get("http://localhost:8000/api/get")
+      .then((response) => {
         setMessage(response.data.message);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1></h1>
       <p>{message}</p>
     </div>
   );
