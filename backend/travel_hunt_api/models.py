d361548e27fb1e03c7257d8969150c68d483b849
@SIA <59891156+SA-Tester@users.noreply.github.com>
2023-12-08 19:49:01 +0530

    Initiating Models

--- backend/travel_hunt_api/models.py
+++ backend/travel_hunt_api/models.py
@@ -1,3 +1,22 @@
 # Create your models here.
 
 from django.db import models
+
+class Locations:
+    def initOpenTripMap():
+        return
+    
+    def initAmadeus():
+        return
+    
+    def initBooking():
+        return
+    
+    def getLatitudeLongitude():
+        return
+    
+    def getTouristAttractions():
+        return
+    
+    def getHotels():
+        return
