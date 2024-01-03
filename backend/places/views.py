e3a980605658b2134e87057ad9803ae975e38d35
Sasith MJ <sasithmjayaweera@gmail.com>
2024-01-03 16:16:40 +0530

    new

--- backend/places/views.py
+++ backend/places/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 import requests
 from django.http import JsonResponse
 
-def weather(request, city):
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20Sydney&key=AIzaSyA3pAqddClbAT-GzSbGaFF6vJgeq3iu6-k".format(city)
+def weather():
+    url ='https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.7937%2C-122.3965&radius=500&type=restaurant&key=AIzaSyA3pAqddClbAT-GzSbGaFF6vJgeq3iu6-k'
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data)
