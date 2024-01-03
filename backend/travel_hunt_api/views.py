fc37ec78510fc6db0f3d1fef0b64556968b8c2bc
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-04 01:43:39 +0530

    Creating view to get country data

--- backend/travel_hunt_api/views.py
+++ backend/travel_hunt_api/views.py
@@ -3,7 +3,24 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Country
+
 
 @api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'Hello, World from Django'})
+def get_country(request, country_name):
+    country = Country.objects.filter(name=country_name).values()
+
+    if(len(country) > 0):
+        # print(country[0])
+
+        return Response(
+            {"id": country[0]["id"],
+             "name": country[0]["name"],
+             "code": country[0]["code"],
+             "description": country[0]["description"],
+             "flag": country[0]["flag"]
+             }
+        )
+
+    else:
+        return Response({"msg": "An error occured"})
