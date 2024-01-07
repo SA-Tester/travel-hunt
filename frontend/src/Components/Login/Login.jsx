7162f59a11c3e6ff15c696bd7b5833064b645e5b
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-08 00:41:32 +0530

    Login interface

---
+++ frontend/src/Components/Login/Login.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Login = () => {
+    return(
+        <div className="grid grid-cols-1 h-screen bg-gradient-to-r from-sky-500 to-indigo-500 content-center">
+            <div className="grid grid-cols-1 container border-4 py-4 px-4 bg-white h-fit lg:w-1/3 md:w-full">
+                <h1 className="text-center font-extrabold text-4xl py-3">Login</h1>
+
+                <label for="email" className="py-3 font-bold">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter Email" className="rounded-lg border-gray-100 focus:border-stone-900"/>
+
+                <label for="password" className="py-3 font-bold">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter Password" className="mb-4 rounded-lg border-gray-100 focus:border-stone-900"/>
+
+                <input type="submit" name="login" value="Login" className="py-3 border-b-4 place-content-center font-semibold bg-cyan-600 text-xl text-white rounded-lg"/>
+
+                <div className="grid grid-cols-2 mt-4">
+                    <a href="/signup" className="font-bold underline text-blue-700">Sign Up</a>
+                    <a href="" className="text-right font-bold underline text-blue-700">Forgot Password?</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
