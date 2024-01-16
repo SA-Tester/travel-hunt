dbb9309e9670aaaf89b20788043542d2aaf4c819
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-16 17:37:45 +0530

    Updating App.js to enable notification class to used anywhere

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Toasts/Layout";
 import Home from "./Components/HomePage/Home";
 import City from "./Components/SearchResults/City";
 import Login from "./Components/Login/Login";
@@ -8,12 +9,14 @@ import Signup from "./Components/Signup/Signup";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/city" element={<City />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/city" element={<City />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
