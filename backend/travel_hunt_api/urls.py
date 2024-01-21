34dec0cef80e86e7b880ce2fb28a30b4a427494d
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-21 14:21:37 +0530

    URLs to get Views of Login and Logout

--- backend/travel_hunt_api/urls.py
+++ backend/travel_hunt_api/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
          views.get_country, name='get_country'),
 
     path('validate_signup',
-         views.validate_signup, name='validate_signup')
+         views.validate_signup, name='validate_signup'),
+
+    path('login',
+         views.LoginView.as_view(), name='login'),
+
+    path('logout', views.LogoutView.as_view(), name="logout")
 ]
