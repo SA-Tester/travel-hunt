ac3b3fbbe5c4e8a87d74915d2058e6f3d4c6b80b
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-21 14:24:08 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Toasts/Layout";
 import Home from "./Components/HomePage/Home";
+import TravllerHome from "./Components/HomePage/TravellerHome";
 import City from "./Components/SearchResults/City";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
+import { Logout } from "./Components/Login/Logout";
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/city" element={<City />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/traveller_home" element={<TravllerHome />} />
         </Routes>
       </Layout>
     </BrowserRouter>
