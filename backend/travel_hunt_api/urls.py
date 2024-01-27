b318885cb7246471f4a9857b25149614e54aae65
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-27 21:55:39 +0530

    url update to have admin signup

--- backend/travel_hunt_api/urls.py
+++ backend/travel_hunt_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views_general
 from . import views_user
+from . import views_admin
 
 urlpatterns = [
     path('check_city/<str:city_name>',
@@ -15,5 +16,8 @@ urlpatterns = [
     path('logout', views_general.LogoutView.as_view(), name="logout"),
 
     path('validate_user_signup',
-         views_user.validate_signup, name='validate_user_signup')
+         views_user.validate_signup, name='validate_user_signup'),
+
+    path('validate_admin_signup',
+         views_admin.validate_signup, name='validate_admin_signup')
 ]
