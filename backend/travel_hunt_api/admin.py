0f506c979a0dc8a478f5ec1c8762d7a03b10a30f
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-27 20:28:25 +0530

    registering new tables to accamodate the admin login

--- backend/travel_hunt_api/admin.py
+++ backend/travel_hunt_api/admin.py
@@ -5,11 +5,15 @@ from .models import Location
 from .models import User
 from .models import Hotel
 from .models import Trip
+from .models import HotelOwner
+from .models import Traveller
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Location)
-admin.site.register(User)
 admin.site.register(Hotel)
 admin.site.register(Trip)
+admin.site.register(User)
+admin.site.register(HotelOwner)
+admin.site.register(Traveller)
