b9480fd903ee4dc4a48d0ac8644074f6487e6d2b
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-27 20:28:54 +0530

    new registered urls for user login

--- backend/travel_hunt_api/urls.py
+++ backend/travel_hunt_api/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from . import views
+from . import views_general
+from . import views_user
 
 urlpatterns = [
-    path('check_city/<str:city_name>', views.check_city, name="check_city"),
-    
-    path('get_city/<str:city_id>',
-         views.get_city, name='get_city'),
+    path('check_city/<str:city_name>',
+         views_general.check_city, name="check_city"),
 
-    path('validate_signup',
-         views.validate_signup, name='validate_signup'),
+    path('get_city/<str:city_id>',
+         views_general.get_city, name='get_city'),
 
     path('login',
-         views.LoginView.as_view(), name='login'),
+         views_general.LoginView.as_view(), name='login'),
 
-    path('logout', views.LogoutView.as_view(), name="logout")
+    path('logout', views_general.LogoutView.as_view(), name="logout"),
+
+    path('validate_user_signup',
+         views_user.validate_signup, name='validate_user_signup')
 ]
