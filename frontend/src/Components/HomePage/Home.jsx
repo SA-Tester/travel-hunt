5bcc77d10ec240024aab1a69003494ce0d8385e7
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-01-29 13:07:40 +0530

    removing unnecessary comments in Home.jsx

--- frontend/src/Components/HomePage/Home.jsx
+++ frontend/src/Components/HomePage/Home.jsx
@@ -1,6 +1,4 @@
 import NavBar from "../NavBar/Navbar";
-import axios from "axios";
-import React, { useState, useEffect } from "react";
 import HeroSection from "../Hero/HeroSection";
 import FooterCon from "../Footer/FooterCon";
 import Places from "../Places/Places";
@@ -10,18 +8,12 @@ import Hotels from "../Places/Hotels";
 const Home = () => {
  
   return (
-    // <div>
-    //   <h3>Restaurant Name: {result.name}</h3>
-    //   {/* Render additional restaurant details here */}
-    // </div>
     <div className="dark:bg-slate-900">
       <NavBar />
       <HeroSection />
       <Places props={{ name: "Popular Places" }} />
       <Hotels />
-
       <FooterCon />
-      {/* <Helloworld /> */}
     </div>
   );
 };
