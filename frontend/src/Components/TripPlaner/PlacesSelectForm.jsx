46d9f58db7181e82ef3a5f8dd20ce8d0e1cee106
Sasith MJ <sasithmjayaweera@gmail.com>
2024-01-29 12:07:13 +0530

    hotelpage

--- frontend/src/Components/TripPlaner/PlacesSelectForm.jsx
+++ frontend/src/Components/TripPlaner/PlacesSelectForm.jsx
@@ -126,9 +126,8 @@ const PlaceSelectForm = ({ tripCallback }) => {
             <button
               type="submit"
               className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 mt-3"
-              onClick={() => {
-                tripCallback(3);
-              }}
+           
+              
             >
               Save Tour
             </button>
