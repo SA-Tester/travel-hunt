0dc9b5a6c74a92366863479f72b87408dd0f3ebb
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-02-28 17:27:08 +0530

    removing unnecessary comments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,15 +96,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
     }
-    # "default": {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'travel_hunt',
-    #     'USER': 'root',
-    #     'PASSWORD': '', 
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
-    # }
 }
 
 
